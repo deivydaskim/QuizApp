99f8bc0cd47a139e8206e5af282bcb6e866def77
deivydas.kim <deivydas.kim@gmail.com>
2025-01-24 23:15:51 +0200

    update layout with header and add styling

--- Quiz.Web/src/pages/MainLayout.tsx
+++ Quiz.Web/src/pages/MainLayout.tsx
@@ -1,13 +1,26 @@
-import { Outlet } from 'react-router';
+import { Outlet, useNavigate } from 'react-router';
+
+import Button from '@/components/Button';
 
 const MainLayout = () => {
+  const navigate = useNavigate();
+
   return (
-    <>
-      <header className="flex h-14 items-center border-b">Header</header>
-      <div>
+    <div className="min-h-screen bg-gradient-to-r from-violet-300 to-pink-300 p-4">
+      <header className="mb-8 flex justify-center">
+        <nav className="flex gap-8">
+          <Button variant="secondary" onClick={() => navigate('/')}>
+            Start Quiz
+          </Button>
+          <Button variant="secondary" onClick={() => navigate('/highscores')}>
+            High Scores
+          </Button>
+        </nav>
+      </header>
+      <div className="mx-auto h-96 max-w-screen-sm rounded-xl bg-white p-4">
         <Outlet />
       </div>
-    </>
+    </div>
   );
 };
 
